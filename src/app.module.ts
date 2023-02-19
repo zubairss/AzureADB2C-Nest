bd59ae3d643e871a7b133116b5814e64c951f4cc
Zubair Shahid <zubairshahid814@gmail.com>
2023-02-19 11:22:52 +0500

    chore: setup fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { validate } from 'class-validator';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, validate })],
+  imports: [ConfigModule.forRoot({ isGlobal: true, validate }), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
